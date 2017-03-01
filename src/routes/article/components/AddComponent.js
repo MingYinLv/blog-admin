fdd37f94b0d47ff3c9ae9f86fadccdc7455b42bc
lvmingyin-mac <lvmingyin@lvmingyindeMacBook-Pro.local>
2017-03-01 15:31:40 +0800

    markdown

--- src/routes/article/components/AddComponent.js
+++ src/routes/article/components/AddComponent.js
@@ -3,22 +3,51 @@
  */
 
 import React, { PropTypes, Component } from 'react';
-
+import marked from 'marked';
 import { shouldComponentUpdate } from 'react-immutable-render-mixin';
 import { Card, Form, Input, Button, Tabs, Icon } from 'antd';
+import { highlightAuto } from 'highlight.js';
+import 'highlight.js/styles/github.css';
+import 'github-markdown-css';
 
 const TabPane = Tabs.TabPane;
 const FormItem = Form.Item;
 
+marked.setOptions({
+  highlight: (code) => {
+    return `<div class="hljs">${highlightAuto(code).value}</div>`;
+  },
+});
+
 class AddComponent extends Component {
-  static propTypes = {}
+  static propTypes = {
+    form: PropTypes.object.isRequired,
+  };
 
   constructor(props) {
     super(props);
     this.shouldComponentUpdate = shouldComponentUpdate.bind(this);
+    this.state = {
+      look: false,
+    };
   }
 
+  onTabsChange = (key) => {
+    let look = false;
+    if (key === '2') {
+      look = true;
+    }
+
+    if (look !== this.state.look) {
+      this.setState({
+        look,
+      });
+    }
+  };
+
   render() {
+    const { getFieldDecorator, getFieldValue } = this.props.form;
+
     return (
       <Card title="添加文章">
         <Form className="login-form">
@@ -31,21 +60,39 @@ class AddComponent extends Component {
           <FormItem>
             <Input addonBefore="文章标签" placeholder="文章标签" />
           </FormItem>
-          <Tabs>
+          <Tabs
+            defaultActiveKey="1"
+            tabBarExtraContent={
+              <Button><Icon type="arrows-alt" /></Button>
+            }
+            onChange={this.onTabsChange}
+          >
             <TabPane key="1" tab={<span><Icon type="edit" />编辑内容</span>}>
               <FormItem>
-                <Input type="textarea" style={{ resize: 'none' }} rows={10} placeholder="文章标签" />
+                {getFieldDecorator('content', {
+                  rules: [{ required: true, message: '请输入文章内容' }],
+                })(
+                  <Input
+                    type="textarea"
+                    rows={20}
+                    placeholder="文章内容"
+                  />,
+                )}
               </FormItem>
             </TabPane>
             <TabPane key="2" tab={<span><Icon type="eye" />预览</span>}>
-              123
+              <div
+                className="markdown-body"
+                style={{ marginBottom: '20px' }}
+                dangerouslySetInnerHTML={{ __html: marked(getFieldValue('content') || '') }}
+              />
             </TabPane>
           </Tabs>
-
+          <Button>保存</Button>
         </Form>
       </Card>
     );
   }
 }
 
-export default AddComponent;
+export default Form.create()(AddComponent);
