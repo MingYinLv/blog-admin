7826e9cd9b5d31d5e7e651241f1f57a500149fd6
lvmingyin <lvmingyin@vip.qq.com>
2017-03-22 19:50:41 +0800

    布局

--- src/routes/Page/index.js
+++ src/routes/Page/index.js
@@ -5,11 +5,13 @@ import Page from './components/Page';
 import ArticleRoute from '../Article';
 import TypeRoute from '../Type';
 import PersonRoute from '../Person';
+import { createUrl } from '../../util/pathUtil';
 
 
 export default store => ({
   path: 'page',
   component: Page,
+  indexRoute: { onEnter: (nextState, replace) => replace(`${createUrl('page/person')}`) },
   childRoutes: [
     ArticleRoute(store),
     TypeRoute(store),
--- src/routes/Person/components/Person.js
+++ src/routes/Person/components/Person.js
@@ -4,10 +4,15 @@
 
 import React, { PropTypes, Component } from 'react';
 import { shouldComponentUpdate } from 'react-immutable-render-mixin';
-import { Card } from 'antd';
+import { Card, Form, Input, Radio, Button } from 'antd';
+
+const RadioGroup = Radio.Group;
+const FormItem = Form.Item;
 
 class Person extends Component {
-  static propTypes = {}
+  static propTypes = {
+    form: PropTypes.object.isRequired,
+  };
 
   constructor(props) {
     super(props);
@@ -15,14 +20,79 @@ class Person extends Component {
   }
 
   render(){
+    const { getFieldDecorator } = this.props.form;
+    const inputCol = {
+      labelCol: { span: 4 },
+      wrapperCol: { span: 14 },
+    };
+
     return (
       <Card
         title="账户信息"
       >
-        123
+        <Form>
+          <FormItem
+            {...inputCol}
+            label="姓名"
+          >
+            {getFieldDecorator('name', {
+              rules: [{ required: true, message: '姓名' }],
+            })(
+              <Input placeholder="姓名" />,
+            )}
+          </FormItem>
+          <FormItem
+            {...inputCol}
+            label="性别"
+          >
+            {getFieldDecorator('sex')(
+              <RadioGroup>
+                <Radio value="男">男</Radio>
+                <Radio value="女">女</Radio>
+                <Radio value="未知">未知</Radio>
+              </RadioGroup>,
+            )}
+          </FormItem>
+          <FormItem
+            {...inputCol}
+            label="年龄"
+          >
+            {getFieldDecorator('name', {
+              rules: [{ required: true, message: '年龄不能为空' }],
+            })(
+              <Input placeholder="年龄" />,
+            )}
+          </FormItem>
+          <FormItem
+            {...inputCol}
+            label="邮箱"
+          >
+            {getFieldDecorator('name', {
+              rules: [{ required: true, message: '邮箱不能为空' }],
+            })(
+              <Input placeholder="邮箱" />,
+            )}
+          </FormItem>
+          <FormItem
+            {...inputCol}
+            label="公司"
+          >
+            {getFieldDecorator('name', {
+            })(
+              <Input placeholder="公司" />,
+            )}
+          </FormItem>
+          <FormItem
+            style={{ marginLeft: '17%' }}
+          >
+            <Button htmlType="submit">
+                保存
+            </Button>
+          </FormItem>
+        </Form>
       </Card>
     );
   }
 }
 
-export default Person;
+export default Form.create()(Person);
